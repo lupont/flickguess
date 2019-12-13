09f919e85ad5ef2326a2de0b1d385c8d88a88153
Pontus Laos <laos.pontus@gmail.com>
2019-12-13 08:59:00 +0100

    Added some basic react examples

--- client/flickguess/src/App.js
+++ client/flickguess/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BazQuz from './components/FooBar';
 
 function App() {
+  // Uses the JS ternary operator to showcase
+  // conditional rendering.
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {true ? <BazQuz/> : <p>Not foo bar</p>}
     </div>
   );
 }
