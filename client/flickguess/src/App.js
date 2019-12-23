402c9874044359050d5e4bb45aa9b9853ced2355
Phatchana srinin <phatchanasrinin@gmail.com>
2019-12-23 01:49:56 +0100

    testade med react.js och css

--- client/flickguess/src/App.js
+++ client/flickguess/src/App.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import './App.css';
 import BazQuz from './components/FooBar';
+import FilmQuiz from './components/FilmQuiz';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  // Uses the JS ternary operator to showcase
-  // conditional rendering.
+
+
+
+class App extends React.Component {
+  /*render(){
+
+ //  Uses the JS ternary operator to showcase
+   //conditional rendering.
   
-  return (
-    <div className="App">
-      {true ? <BazQuz/> : <p>Not foo bar</p>}
+    return (
+     <div className="App">
+        {true ? <BazQuz/> : <p>Not foo bar</p>}
+      </div>
+      );
+    }
+  }*/
+
+
+  render(){
+    
+    return (
+      <div className="App">
+      <FilmQuiz />
     </div>
-  );
+    );
+  }
+
 }
 
+
+
+
+
+
 export default App;
+
---
+++ client/flickguess/src/components/quizData.js
@@ -0,0 +1,29 @@
+// inte den databasen vi kommer att använda, mer för det ska vara lättare för att designa
+
+export const quizData = [
+    {
+      id: 0,
+      question: ` Vilken film är detta ?`,
+      options: [`Star Wars`, `The Hobbit`, `Ghostbusters`],
+      answer: `The Hobbit`
+    },
+    {
+      id: 1,
+      question: `Vilken film är detta?`,
+      options: [`The Ring`, `Greese`, `Lion King`],
+      answer: `Lion King`
+    },
+    {
+      id: 3,
+      question: `Vilken film är detta?`,
+      options: [`Breakfast Club`, `Inception`, `IT`],
+      answer: `Inception`
+    },
+    {
+      id: 4,
+      question: `Vilken film är detta?`,
+      options: [`Dark Knight`, `Mulan`, `Gladiator`],
+      answer: `Mulan`
+    }
+  ];
+  
